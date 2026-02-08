23c96953f13bbb433ba2d2f00964aadbf5fd245e
Greg Nazario <greg@gnazar.io>
2026-02-08 01:19:53 -0500

    Add Highlight.js v10 support

---
+++ src/languages/move.mjs
@@ -0,0 +1,6 @@
+/**
+ * ESM wrapper for the Move highlight.js grammar (v11+ API).
+ * @type {import('highlight.js').LanguageFn}
+ */
+import move from './move.js';
+export default move;
---
+++ src/languages/move.v10.mjs
@@ -0,0 +1,5 @@
+/**
+ * ESM wrapper for the Move highlight.js grammar (v10 API).
+ */
+import move from './move.v10.js';
+export default move;
